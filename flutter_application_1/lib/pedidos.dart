e6f5af1992d59484f7bbb4c952f16dd9aecaa1ea
pamela <pamela.oliveira219@etec.sp.gov.br>
2022-11-16 19:43:15 -0300

    CRUD

---
+++ flutter_application_1/lib/pedidos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Pedidos extends StatefulWidget {
+  const Pedidos({Key? key}) : super(key: key);
+
+  @override
+  State<Pedidos> createState() => _PedidosState();
+}
+
+class _PedidosState extends State<Pedidos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ flutter_application_1/lib/produtos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Produtos extends StatefulWidget {
+  const Produtos({Key? key}) : super(key: key);
+
+  @override
+  State<Produtos> createState() => _ProdutosState();
+}
+
+class _ProdutosState extends State<Produtos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
